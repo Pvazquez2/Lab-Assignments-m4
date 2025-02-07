59299e85261a9ed326f881c65041a3a0c173be8d
Pvazquez2 <pvazquez2@my.nl.edu>
2025-02-07 14:42:31 -0600

    Add files via upload

---
+++ time debugged.py
@@ -0,0 +1,9 @@
+currentTimeStr = input("What is the current time (in hours 0-23)? ")
+waitTimeStr = input("How many hours do you want to wait?")
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int (waitTimeStr)
+
+finalTimeInt = (currentTimeInt + waitTimeInt) % 24
+
+print("The time after waiting will be:" , finalTimeInt)
